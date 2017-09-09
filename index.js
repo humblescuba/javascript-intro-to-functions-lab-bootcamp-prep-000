c005035be302f02c65132df1dbb824baff973045
Steve Olender <nantucket.friar@gmail.com>
2017-09-09 02:53:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 
+var upperCase = string.toUpperCase()
+var lowerCase = string.toLowerCase()
+
 function sayHiToGrandma(string) {
-  
+  if string === lowerCase
+    console.log("I can\'t hear you!");
+  if string === upperCase
+    console.log("YES INDEED!");
+  if string = "I love you, Grandma."
+    console.log("I love you, too.");
 }
